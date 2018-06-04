a2c2eb63735a27a4214ec9517bcec5cec26eb0fa
kakkarotssj <kanjeckcooldeva.005@gmail.com>
2018-06-04 10:35:59 +0530

    with new vertices

--- graph_algos/topological_sort.py
+++ graph_algos/topological_sort.py
@@ -31,13 +31,25 @@ class Graph:
 		for n1, n2 in self.graph.items():
 			print n1, n2
 
+# graph = Graph(6)
+# graph.add_edge(5, 2)
+# graph.add_edge(5, 0)
+# graph.add_edge(4, 0)
+# graph.add_edge(4, 1)
+# graph.add_edge(2, 3)
+# graph.add_edge(3, 1)
+
 graph = Graph(6)
-graph.add_edge(5, 2)
-graph.add_edge(5, 0)
-graph.add_edge(4, 0)
-graph.add_edge(4, 1)
+graph.add_edge(0, 1)
+graph.add_edge(0, 2)
 graph.add_edge(2, 3)
-graph.add_edge(3, 1)
+graph.add_edge(2, 4)
+graph.add_edge(2, 5)
+graph.add_edge(3, 4)
+graph.add_edge(3, 5)
+graph.add_edge(1, 3)
+graph.add_edge(1, 2)
+graph.add_edge(4, 5)
 
 graph.print_graph()
 
