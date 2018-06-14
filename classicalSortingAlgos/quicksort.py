d575a02eb49dcdb725d31eba6b3c03ef631e3855
kakkarotssj <kanjeckcooldeva.005@gmail.com>
2018-06-15 00:09:55 +0530

    quicksort

---
+++ classicalSortingAlgos/quicksort.py
@@ -0,0 +1,20 @@
+def partition(arr, l, r):
+	pivot = arr[r]
+	wall = l-1
+	for i in range(l, r):
+		if arr[i] >= pivot:
+			pass
+		else:
+			arr[i], arr[wall+1] = arr[wall+1], arr[i]
+			wall += 1
+	arr[wall+1], arr[r] = pivot, arr[wall+1]
+	return wall+1
+def quicksort(arr, l , r):
+	if l < r:
+		sortedindex = partition(arr, l, r)
+		quicksort(arr, l, sortedindex-1)
+		quicksort(arr, sortedindex+1, r)	
+arr = [12, 11, 13, 5, 6, 7]
+l, r = 0, len(arr)-1
+quicksort(arr, l, r)
+print arr
